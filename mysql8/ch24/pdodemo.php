770e2f68068c2d7e602ce6ad221a3b2f73136491
lirongyaoper <lirongyaoper@163.com>
2025-09-12 18:08:24 +0800

    update data at 20250912180824

--- mysql8/ch24/pdodemo.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-   $dbconnect = new PDO('mysql:host=localhost;dbname=pdodatabase','root',' ');
-} catch (PDOException $exception) {
-   echo "Connection error message: " . $exception->getMessage();
-}
-?>
